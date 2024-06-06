a35221f1633a087b9fffef728f58acfab81d75ca
Victor Rayan <victor.rayan378@gmail.com>
2024-06-06 15:46:01 -0300

    add jwt logic and password hash
    Co-Authored-By: Mateus Moreira <49012681+mateus-lm@users.noreply.github.com>

---
+++ app/api/deps.py
@@ -0,0 +1,19 @@
+from collections.abc import AsyncGenerator
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api import api_messages
+from app.core import database_session
+from app.core.security.jwt import verify_jwt_token
+from app.models import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/access-token")
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with database_session.get_async_session() as session:
+        yield session
