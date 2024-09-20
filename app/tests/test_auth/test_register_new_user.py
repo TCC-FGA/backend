d700c79797255aaf713738fa976a5fd6aab9e61c
Victor Rayan <victor.rayan378@gmail.com>
2024-09-20 19:32:04 -0300

    feat: add tests base code
    Co-Authored-By: Mateus Moreira <49012681+mateus-lm@users.noreply.github.com>

---
+++ app/tests/test_auth/test_register_new_user.py
@@ -0,0 +1,85 @@
+from datetime import date
+from fastapi import status
+from httpx import AsyncClient
+from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.controllers.api import api_messages
+from app.main import app
+from app.models.models import Owner as User
+import pytest
+
+@pytest.mark.asyncio
+async def test_register_new_user_status_code(
+    client: AsyncClient,
+) -> None:
+    response = await client.post(
+        app.url_path_for("register_new_user"),
+        json={
+            "email": "test@email.com",
+            "password": "testtesttest",
+            "name": "Test User",
+            "telephone": "1234567890",
+            "hashed_signature": "hashed_signature",
+            "cpf": "12345678901",
+            "birth_date": "1990-01-01",
+        },
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+@pytest.mark.asyncio
+async def test_register_new_user_creates_record_in_db(
+    client: AsyncClient,
+    session: AsyncSession,
+) -> None:
+    await client.post(
+        app.url_path_for("register_new_user"),
+        json={
+            "email": "test@email.com",
+            "password": "testtesttest",
+            "name": "Test User",
+            "telephone": "1234567890",
+            "cpf": "12345678901",
+            "birth_date": "1990-01-01",
+            "hashed_signature": "hashed_signature",
+        },
+    )
+
+    user_count = await session.scalar(
+        select(func.count()).where(User.email == "test@email.com")
+    )
+    assert user_count == 1
+
+@pytest.mark.asyncio
+async def test_register_new_user_cannot_create_already_created_user(
+    client: AsyncClient,
+    session: AsyncSession,
+) -> None:
+    user = User(
+        email="test@email.com",
+        hashed_password="hashedpassword",
+        name="Test User",
+        telephone="1234567890",
+        cpf="12345678901",
+        hashed_signature="hashed_signature",
+        birth_date=date(1990, 1, 1),
+    )
+    session.add(user)
+    await session.commit()
+
+    response = await client.post(
+        app.url_path_for("register_new_user"),
+        json={
+            "email": "test@email.com",
+            "password": "testtesttest",
+            "name": "Test User",
+            "telephone": "1234567890",
+            "cpf": "12345678901",
+            "birth_date": "1990-01-01",
+            "hashed_signature": "hashed_signature",
+        },
+    )
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"detail": api_messages.EMAIL_ADDRESS_ALREADY_USED}
